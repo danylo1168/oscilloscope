8c0b5423ad49770938ff5f34fc04363983c4889b
1168 <danylohnativ8611@gmail.com>
2026-08-04 13:21:37 +0300

    Oscilloscope functions

--- Core/Src/oscilloscope.c
+++ Core/Src/oscilloscope.c
@@ -4,10 +4,11 @@ static State current_state = OSC_WAIT_DATA;
 static uint16_t adc_buffer[4096];
 static uint16_t trigger_index = 0;
 static uint8_t buffer_flag = 0;
+static uint16_t old_y[320] = {0};
 
 void OscilloscopeUpdate(void);
 void OscilloscopeToggleRunStop(void);
-static void OsclilloscopeTrigger(void);
+static void OscilloscopeTrigger(void);
 static void OscilloscopeSendData(void);
 void HAL_ADC_ConvHalfCpltCallback (ADC_HandleTypeDef * hadc1);
 void HAL_ADC_ConvCpltCallback (ADC_HandleTypeDef * hadc1);
@@ -19,7 +20,7 @@ void OscilloscopeUpdate()
 	case OSC_WAIT_DATA:
 		break;
 	case OSC_FIND_TRIGGER:
-		OsclilloscopeTrigger();
+		OscilloscopeTrigger();
 		break;
 	case OSC_DRAW_SCREEN:
 		OscilloscopeSendData();
@@ -27,6 +28,29 @@ void OscilloscopeUpdate()
 	case OSC_PAUSED:
 		break;
 	}
+}
+
+static void OscilloscopeSendData()
+{
+	uint16_t raw_value = 0;
+	uint16_t y = 0;
+	for (int i = 0; i < 320; i++) // X = i
+	{
+		raw_value = adc_buffer[trigger_index + i];
+
+		// 0 < raw_value < 4096
+		// 4096/240 = 256/15
+	    // (raw_value * 15) / 256
+		// 240 - (raw_value * 15) / 256 to flip Y coordinate
+		y = 240 - ((raw_value * 15) >> 8);
+
+		// Draw new frame
+		// DrawScreen(i, old_y[i], BLACK);
+		// DrawScreen (i, y, GREEN);
+		// old_y[i] = y;
+
+	}
+	current_state = OSC_WAIT_DATA;
 }
 
 void HAL_ADC_ConvHalfCpltCallback (ADC_HandleTypeDef *hadc1)
@@ -61,7 +85,7 @@ void OscilloscopeToggleRunStop()
 	}
 }
 
-void OscilloscopeTrigger()
+static void OscilloscopeTrigger()
 {
 	// Trigger was found
 	uint16_t start_index = (buffer_flag == 0) ? 1 : 2049;
